3d28eb47559a3b926d2c783c50b93738fcf28804
Evan Capella <evancapella@gmail.com>
2017-10-19 17:25:24 -0400

    updated some file names and created a start script

--- angular-src/src/app/Album.ts
+++ angular-src/src/app/Album.ts
@@ -1,4 +1,4 @@
-import { Song } from './song';
+import { Song } from './Song';
 
 export class Album{
     type: string;
---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Initial startup script
+
+echo "[INFO] Installing express dependencies"
+npm install
+echo "[INFO] Installing angular dependencies"
+cd angular-src && npm install
+cd ..
+if [ fuser 3000/tcp ]; then
+	echo "[INFO] Killing existing express server"
+	fuser -k 3000/tcp
+fi
+echo "[INFO] Starting fresh express server"
+node server &
+echo "[INFO] Starting angular server"
+cd angular-src && npm start
