cffcf596afa5d905b9f5f32c6c63c7a2bafa92eb
Ethan Della Posta <ejd3081@rit.edu>
2017-11-07 09:44:41 -0500

    top bar

--- angular-src/src/app/components/artists/artists.component.ts
+++ angular-src/src/app/components/artists/artists.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ArtistService } from '../../services/artist.service';
 import { Artist } from '../../Artist';
 import { Album } from '../../Album';
+import { Song } from '../../Song';
 
 @Component({
   moduleId: module.id,
@@ -13,6 +14,7 @@ export class ArtistsComponent {
     artists: Artist[];
     title: string;
     albums: Album[] = [];
+    songs: Song[] = [];
 
     getAlbums(artists){
         let i=0;
@@ -25,10 +27,31 @@ export class ArtistsComponent {
                 temp_albums[i].ratings = album.ratings;
                 temp_albums[i].title = album.title;
                 temp_albums[i].songs = album.songs;
+                temp_albums[i].artist = album.artist;
                 i++;
             }
         } 
         return temp_albums; 
+    }
+
+    getSongs(artists){
+        let i=0;
+        let temp_songs = [];
+        for (const artist of artists){
+            for(const album of artist.albums){
+                for (const song of album.songs){
+                    temp_songs[i] = new Song();
+                    temp_songs[i].title = song.title;
+                    temp_songs[i].lyrics = song.lyrics;
+                    temp_songs[i].length = song.length;
+                    temp_songs[i].link = song.link;
+                    temp_songs[i].artist = song.artist;
+                    temp_songs[i].album = song.album;
+                    
+                }
+            }
+        } 
+        return temp_songs; 
     }
 
 
@@ -43,6 +66,7 @@ export class ArtistsComponent {
             .subscribe(artists => {
                 this.artists = artists;
                 this.albums = this.getAlbums(artists);
+                this.songs = this.getSongs(artists);
             });
     }
 }
