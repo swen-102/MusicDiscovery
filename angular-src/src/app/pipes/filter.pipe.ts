fca319b9bb430db26ecb49cb5085f84179ca9a55
Ethan Della Posta <ejd3081@rit.edu>
2017-12-07 14:49:51 -0500

    Search works for all three models

--- angular-src/src/app/pipes/filter.pipe.ts
+++ angular-src/src/app/pipes/filter.pipe.ts
@@ -1,17 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Artist } from '../Artist'
+import { Album } from '../Album'
+import { Song } from '../Song'
+
 
 @Pipe({
     name: 'filter'
 })
 
 export class FilterPipe implements PipeTransform {
-    transform(items: Artist[], searchText: string): Artist[] {
+    transform(items: Array<any>, searchText: string): Array<any> {
         if (!items) return [];
         if (!searchText) return items;
         searchText = searchText.toLowerCase();
         return items.filter(it => {
-            return it.name.toLowerCase().includes(searchText);
+            if(it.name)
+              return it.name.toLowerCase().includes(searchText);
+            else if(it.title)
+              return it.title.toLowerCase().includes(searchText);
+
         });
     }
 }
